7f4c196faf685a0483b8887559e996fa3361c9b6
Shirley Katherine <kathysk29@gmail.com>
2026-07-08 12:30:03 +0530

    Add resume ingestion and ChromaDB storage

--- ingest.py
+++ ingest.py
@@ -1,6 +1,6 @@
 import os
 from pypdf import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
